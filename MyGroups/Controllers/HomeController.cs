53e81f4b14856d603066de22a5c109a8b5a3f655
Jan Hajek <hajek.j@hotmail.com>
2017-08-25 13:01:45 +0200

    Add more info for groups.

--- MyGroups/Controllers/HomeController.cs
+++ MyGroups/Controllers/HomeController.cs
@@ -28,32 +28,53 @@ namespace MyGroups.Controllers
         }
         public async Task<IActionResult> Index()
         {
+            var tenantList = await _asm.GetTenantsListCachedAsync();
+            var currentTenantId = HttpContext.User.FindFirst(AzureAdClaimTypes.TenantId)?.Value;
+            var currentTenant = tenantList.Tenants.Where(x => x.Id == currentTenantId).First();
+
             var graphClient = _graph.GetClient();
             List<GroupDTO> groups = new List<GroupDTO>();
 
             var currentGroups = (await graphClient.Me.MemberOf.Request().GetAsync()).CurrentPage.OfType<Group>().Where(x => x.GroupTypes.Contains("Unified"));
 
-            var results = new ConcurrentBag<Site>();
+            var results = new ConcurrentBag<(Group group, Site site, IOnenoteNotebooksCollectionPage notebooks, IPlannerGroupPlansCollectionPage plans)>();
 
             await currentGroups.ParallelForEachAsync(
                 async currentGroup =>
                 {
                     var site = await graphClient.Groups[currentGroup.Id].Sites["root"].Request().GetAsync();
-                    results.Add(site);
+                    var onenote = await graphClient.Groups[currentGroup.Id].Onenote.Notebooks.Request().GetAsync();
+                    var planner = await graphClient.Groups[currentGroup.Id].Planner.Plans.Request().GetAsync();
+                    results.Add((currentGroup, site, onenote, planner));
                 }, 10);
 
-            foreach(var currentSite in results)
+            foreach(var result in results)
             {
                 GroupDTO group = new GroupDTO();
-                group.Name = currentSite.DisplayName;
-                group.SharePointUrl = currentSite.WebUrl;
+                group.Name = result.group.DisplayName;
+                group.SharePointUrl = result.site.WebUrl;
+                foreach(var nb in result.notebooks)
+                {
+                    group.Notebooks.Add(new OneNoteDTO
+                    {
+                        Name = nb.DisplayName,
+                        Url = nb.Links.OneNoteWebUrl.Href
+                    });
+                }
+                foreach (var p in result.plans)
+                {
+                    group.Plans.Add(new PlanDTO
+                    {
+                        Name = p.Title,
+                        Url = $"https://tasks.office.com/{currentTenant.DomainName}/Home/Planner#/plantaskboard?groupId={result.group.Id}&planId={p.Id}"
+                    });
+                }
                 groups.Add(group);
             }
 
             groups = groups.OrderBy(x => x.Name).ToList();
 
-            var currentTenantId = HttpContext.User.FindFirst(AzureAdClaimTypes.TenantId)?.Value;
-            ViewData["tenant"] = (await _asm.GetTenantsListCachedAsync()).Tenants.Where(x => x.Id == currentTenantId).First();
+            ViewData["tenant"] = currentTenant;
 
             return View(groups);
         }
